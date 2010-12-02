8126c7e7518af9c18c9f13ab98906697700607e3
Felix Bechstein <f@ub0r.de>
2010-12-02 21:16:58 +0100

    initial commit

---
+++ src/de/ub0r/android/missingkeys/Main.java
@@ -0,0 +1,117 @@
+package de.ub0r.android.missingkeys;
+
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.ClipboardManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Main extends Activity implements OnClickListener,
+		OnLongClickListener {
+
+	/** Prefix ins {@link SharedPreferences}. */
+	private static final String PREFS_PREFIX = "btn_";
+
+	/** Buttons pid for R.id. */
+	private final HashMap<Integer, String> btns = new HashMap<Integer, String>();
+
+	/**
+	 * Load a {@link Button}.
+	 * 
+	 * @param id
+	 *            R.id
+	 * @param pid
+	 *            id in {@link SharedPreferences}
+	 */
+	private void loadBtn(final int id, final String pid,
+			final SharedPreferences p) {
+		final Button v = (Button) this.findViewById(id);
+		v.setOnClickListener(this);
+		v.setOnLongClickListener(this);
+		final String rpid = PREFS_PREFIX + pid;
+		final String s = p.getString(rpid, "");
+		if (s.length() > 0) {
+			v.setText(s);
+		}
+		this.btns.put(id, rpid);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.main);
+
+		final SharedPreferences p = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		this.loadBtn(R.id.btn_1_1, "1_1", p);
+		this.loadBtn(R.id.btn_1_2, "1_2", p);
+		this.loadBtn(R.id.btn_1_3, "1_3", p);
+		this.loadBtn(R.id.btn_1_4, "1_4", p);
+		this.loadBtn(R.id.btn_1_5, "1_5", p);
+		this.loadBtn(R.id.btn_1_6, "1_6", p);
+		this.loadBtn(R.id.btn_2_1, "2_1", p);
+		this.loadBtn(R.id.btn_2_2, "2_2", p);
+		this.loadBtn(R.id.btn_2_3, "2_3", p);
+		this.loadBtn(R.id.btn_2_4, "2_4", p);
+		this.loadBtn(R.id.btn_2_5, "2_5", p);
+		this.loadBtn(R.id.btn_2_6, "2_6", p);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onClick(final View v) {
+		if (v instanceof Button) {
+			final Button btn = (Button) v;
+			final ClipboardManager cbmgr = (ClipboardManager) this
+					.getSystemService(CLIPBOARD_SERVICE);
+			cbmgr.setText(btn.getText());
+			this.finish();
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean onLongClick(final View v) {
+		if (v instanceof Button) {
+			final Button btn = (Button) v;
+			final AlertDialog.Builder b = new AlertDialog.Builder(this);
+			b.setTitle(R.string.edit_);
+			b.setCancelable(true);
+			final EditText et = new EditText(this);
+			et.setText(btn.getText());
+			b.setView(et);
+			b.setNegativeButton(android.R.string.cancel, null);
+			b.setPositiveButton(android.R.string.ok,
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(final DialogInterface dialog,
+								final int which) {
+							final String text = et.getText().toString();
+							btn.setText(text);
+							PreferenceManager.getDefaultSharedPreferences(
+									Main.this).edit().putString(
+									Main.this.btns.get(v.getId()), text)
+									.commit();
+						}
+					});
+			b.show();
+		}
+		return false;
+	}
+}
